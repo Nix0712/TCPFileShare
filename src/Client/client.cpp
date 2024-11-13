c88b1222e0f72f7628390f2d530d084805f1e807
Nix0712 <jokovicnikola06@gmail.com>
2024-11-13 16:45:21 +0100

    Imported boost lib

--- src/Client/client.cpp
+++ src/Client/client.cpp
@@ -5,8 +5,8 @@ using boost::asio::ip::tcp;
 
 int main(int argc, char *argv[]) {
   try {
-    if (argc != 4) {
-      std::cerr << "Usage: client <host> <send/recieve> <file>" << std::endl;
+    if (argc != 2) {
+      std::cerr << "Usage: client <host>" << std::endl;
       return 1;
     }
 
